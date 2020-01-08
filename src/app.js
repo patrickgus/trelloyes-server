9a9a4c9559435f719061e38ec598a6c3e90a30e4
Patrick Gustafson <patrickgus@charter.net>
2020-01-07 20:39:37 -0800

    add error handler middleware

--- src/app.js
+++ src/app.js
@@ -44,6 +44,16 @@ const lists = [
   }
 ];
 
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
