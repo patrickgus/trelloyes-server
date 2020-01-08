a535dda34528eb531b38bd6577b4a43d74de684f
Patrick Gustafson <patrickgus@charter.net>
2020-01-07 21:53:47 -0800

    create routers for card and list modules

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
-const winston = require("winston");
-const uuid = require("uuid/v4");
 const { NODE_ENV } = require("./config");
+const cardRouter = require("./card/card-router");
+const listRouter = require("./list/list-router");
 
 const app = express();
 
@@ -14,37 +14,6 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
-
-const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.json(),
-  transports: [new winston.transports.File({ filename: "info.log" })]
-});
-
-if (NODE_ENV !== "production") {
-  logger.add(
-    new winston.transports.Console({
-      format: winston.format.simple()
-    })
-  );
-}
-
-const cards = [
-  {
-    id: 1,
-    title: "Task One",
-    content: "This is card one"
-  }
-];
-
-const lists = [
-  {
-    id: 1,
-    header: "List One",
-    cardIds: [1]
-  }
-];
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
@@ -57,109 +26,9 @@ app.use(function validateBearerToken(req, res, next) {
   next();
 });
 
-app.get("/card", (req, res) => {
-  res.json(cards);
-});
+app.use(cardRouter);
 
-app.get("/list", (req, res) => {
-  res.json(lists);
-});
-
-app.get("/card/:id", (req, res) => {
-  const { id } = req.params;
-  const card = cards.find(c => c.id == id);
-
-  if (!card) {
-    logger.error(`Card with id ${id} not found.`);
-    return res.status(404).send("Card Not Found");
-  }
-
-  res.json(card);
-});
-
-app.get("/list/:id", (req, res) => {
-  const { id } = req.params;
-  const list = lists.find(li => li.id == id);
-
-  if (!list) {
-    logger.error(`List with id ${id} not found.`);
-    return res.status(404).send("List Not Found");
-  }
-
-  res.json(list);
-});
-
-app.post("/card", (req, res) => {
-  const { title, content } = req.body;
-
-  if (!title) {
-    logger.error(`Title is required`);
-    return res.status(400).send("Invalid data");
-  }
-
-  if (!content) {
-    logger.error(`Content is required`);
-    return res.status(400).send("Invalid data");
-  }
-
-  const id = uuid();
-
-  const card = {
-    id,
-    title,
-    content
-  };
-
-  cards.push(card);
-
-  logger.info(`Card with id ${id} created`);
-
-  res
-    .status(201)
-    .location(`http://localhost:8000/card/${id}`)
-    .json(card);
-});
-
-app.post("/list", (req, res) => {
-  const { header, cardIds = [] } = req.body;
-
-  if (!header) {
-    logger.error(`Header is required`);
-    return res.status(400).send("Invalid data");
-  }
-
-  if (cardIds.length > 0) {
-    let valid = true;
-    cardIds.forEach(cid => {
-      const card = cards.find(c => c.id == cid);
-      if (!card) {
-        logger.error(`Card with id ${cid} not found in cards array.`);
-        valid = false;
-      }
-    });
-
-    if (!valid) {
-      return res.status(400).send("Invalid data");
-    }
-  }
-
-  const id = uuid();
-
-  const list = {
-    id,
-    header,
-    cardIds
-  };
-
-  lists.push(list);
-
-  logger.info(`List with id ${id} created`);
-
-  res
-    .status(201)
-    .location(`http://localhost:8000/list/${id}`)
-    .json({ id });
-});
+app.use(listRouter);
 
 app.use((error, req, res, next) => {
   let response;
